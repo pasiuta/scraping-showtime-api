a959d9bbf8c0842a482f47dc2ee076e0e2f4c546
pasiuta <pasuta.dima@gmail.com>
2023-12-03 12:55:45 +0200

    add error handling for duplicate showtimeId

--- src/showtime/showtime.service.ts
+++ src/showtime/showtime.service.ts
@@ -6,70 +6,67 @@ import { ShowtimeSummaryEntity } from './entity/showtimeSummary.entity';
 
 @Injectable()
 export class ShowtimeService {
-  constructor(
-    @InjectRepository(ShowtimeEntity)
-    private showtimeEntityRepository: Repository<ShowtimeEntity>,
-    @InjectRepository(ShowtimeSummaryEntity)
-    private showtimeSummaryEntityRepository: Repository<ShowtimeSummaryEntity>,
-    private dataSource: DataSource,
-  ) {}
+    constructor(
+        @InjectRepository(ShowtimeEntity)
+        private showtimeEntityRepository: Repository<ShowtimeEntity>,
+        @InjectRepository(ShowtimeSummaryEntity)
+        private showtimeSummaryEntityRepository: Repository<ShowtimeSummaryEntity>,
+        private dataSource: DataSource,
+    ) {}
 
-  private async updateShowtimeSummary() {
-    await this.dataSource.query(`
-        INSERT INTO "showtime-summary"
-        ("showtimeDate",
-         "cinemaName",
-         "movieTitle",
-         attributes,
-         city,
-         "showtimeCount")
-        select date(showtime."showtimeInUTC" AT TIME ZONE 'UTC'),
-            showtime."cinemaName",
-            showtime."movieTitle",
-            showtime.attributes,
-            showtime.city,
-            count(*)
-        from "showtime"
-        group by 1, 2, 3, 4, 5
-        ON CONFLICT
-            (
-            "showtimeDate",
-            "cinemaName",
-            "movieTitle",
-            attributes,
-            city
-            )
-            DO UPDATE
-                   SET "showtimeCount"= EXCLUDED."showtimeCount"
-    `);
-    //TODO: Investigate and resolve the duplication issue in the "showtime-summary" table.
-    // If you check the "showtime-summary" table rows you will notice that there duplicate rows.
-    // Analyze the current aggregation query to identify why duplicates are being created.
-    // Modify the query or the table structure as necessary to prevent duplicate entries.
-    // Ensure your solution maintains data integrity and optimizes performance.
-  }
 
-  async addShowtimes(showtimes: ShowtimeInterface[]) {
-    for (const showtime of showtimes) {
-      await this.dataSource
-        .createQueryBuilder()
-        .insert()
-        .into(ShowtimeEntity)
-        .values({
-          showtimeId: showtime.showtimeId,
-          movieTitle: showtime.movieTitle,
-          cinemaName: showtime.cinemaName,
-          showtimeInUTC: showtime.showtimeInUTC,
-          bookingLink: showtime.bookingLink,
-          attributes: showtime.attributes,
-        })
-        .execute();
+    private async updateShowtimeSummary() {
+        await this.dataSource.query(`
+    INSERT INTO "showtime-summary"
+    ("showtimeDate",
+     "cinemaName",
+     "movieTitle",
+     attributes,
+     city,
+     "showtimeCount")
+    select DISTINCT date(showtime."showtimeInUTC" AT TIME ZONE 'UTC'),
+        showtime."cinemaName",
+        showtime."movieTitle",
+        showtime.attributes,
+        showtime.city,
+        count(*)
+    from "showtime"
+    group by 1, 2, 3, 4, 5
+    ON CONFLICT
+        (
+        "showtimeDate",
+        "cinemaName",
+        "movieTitle",
+        attributes,
+        city
+        )
+        DO UPDATE
+               SET "showtimeCount"= EXCLUDED."showtimeCount"
+`);
 
-      //TODO: Implement error handling for cases where a duplicate 'showtimeId' is used during insertion.
-      // Consider how the application should behave in this scenario (e.g., skip, replace, or abort the operation).
-      // Implement the necessary logic and provide feedback or logging for the operation outcome.
-      // Ensure your solution handles such conflicts gracefully without causing data inconsistency or application failure.
+        //TODO: Investigate and resolve the duplication issue in the "showtime-summary" table.
+        // If you check the "showtime-summary" table rows you will notice that there duplicate rows.
+        // Analyze the current aggregation query to identify why duplicates are being created.
+        // Modify the query or the table structure as necessary to prevent duplicate entries.
+        // Ensure your solution maintains data integrity and optimizes performance.
     }
-    await this.updateShowtimeSummary();
-  }
+
+
+
+
+
+    async addShowtimes(showtimes: ShowtimeInterface[]) {
+        for (const showtime of showtimes) {
+            const existingShowtime = await this.showtimeEntityRepository.findOne({where: { showtimeId: showtime.showtimeId }});
+            if (!existingShowtime) {
+                await this.showtimeEntityRepository.save(showtime);
+            } else {
+                console.warn(`Skipping duplicate entry with showtimeId: ${showtime.showtimeId}`);
+            }
+        }
+        await this.updateShowtimeSummary();
+    }
+
+
+
 }
