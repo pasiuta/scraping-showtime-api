0e25f021e1a2f2ddc6fbbb19e903100f29994be5
pasiuta <pasuta.dima@gmail.com>
2023-12-03 13:28:35 +0200

    extend swagger for scraper response

--- src/scraper/dto/scraper-response.dto.ts
+++ src/scraper/dto/scraper-response.dto.ts
@@ -1,12 +1,17 @@
 import { WebsiteData } from '../interface/website-data.interface';
+import {ApiProperty} from "@nestjs/swagger";
 
 export class ScraperResponseDto {
-  // TODO: As part of this task, you are required to enhance the Swagger documentation for the 'ScraperResponseDto' class.
-  //  1. Apply appropriate Swagger decorators to the 'requestUrl' and 'responseData' fields. Make sure to include the 'description' and 'example' fields in the decorators, providing clear and accurate information.
-  //  2. The 'responseData' field contains data of type 'WebsiteData'. You need to ensure that the structure and content of this type are well-documented in Swagger. Consider the best approach to document the nested fields and types within 'WebsiteData'.
-  //  3. Review the overall Swagger setup for this DTO. Consider if there are any additional details or specific aspects of the data that API consumers would need to know. Your goal is to make the API's response structure and data as clear as possible for anyone reviewing the generated Swagger documentation.
-  //  Please ensure your documentation is comprehensive and follows any standard best practices for API documentation.
 
+  @ApiProperty({
+    description: 'The URL of the website that was scraped',
+    example: 'https://www.example.com',
+  })
   requestUrl: string;
+
+  @ApiProperty({
+    description: 'The data scraped from the requested URL',
+    type: WebsiteData,
+  })
   responseData: WebsiteData;
 }
--- src/scraper/interface/website-data.interface.ts
+++ src/scraper/interface/website-data.interface.ts
@@ -1,9 +1,45 @@
-export interface WebsiteData {
+import {ApiProperty} from "@nestjs/swagger";
+
+export class WebsiteData {
+
+  @ApiProperty({
+    description: 'The title of the website',
+    example: 'My Awesome Website'
+  })
   title: string;
+
+  @ApiProperty({
+    description: 'The meta description of the website, used for SEO',
+    example: 'This is an example meta description for a website'
+  })
   metaDescription: string;
+
+  @ApiProperty({
+    description: 'URL of the website\'s favicon',
+    example: 'https://www.example.com/favicon.ico'
+  })
   faviconUrl: string;
+
+  @ApiProperty({
+    description: 'Array of URLs for scripts included in the website',
+    example: ['https://www.example.com/script1.js', 'https://www.example.com/script2.js']
+  })
   scriptUrls: string[];
+
+  @ApiProperty({
+    description: 'Array of URLs for stylesheets used in the website',
+    example: ['https://www.example.com/style1.css', 'https://www.example.com/style2.css']
+  })
   stylesheetUrls: string[];
+
+  @ApiProperty({
+    description: 'Array of URLs for images found in the website',
+    example: ['https://www.example.com/image1.jpg', 'https://www.example.com/image2.png']
+  })
   imageUrls: string[];
+
+  @ApiProperty({
+    description: 'Array of showtimes, each represented by a ShowtimeInterface object',
+  })
   showtimes: ShowtimeInterface[];
 }
